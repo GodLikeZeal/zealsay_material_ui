7945e6a01e66e73e9e724ad47e22cef053476867
zhanglei <zhanglei@i-counting.cn>
2019-03-06 11:42:34 +0800

    [feat] 父子组建参数传递

--- src/views/userList/components/info.vue
+++ src/views/userList/components/info.vue
@@ -1,6 +1,6 @@
 <template>
     <v-layout>
-        <v-flex >
+        <v-flex>
             <v-card>
                 <v-img
                         class="white--text"
@@ -10,26 +10,29 @@
                     <v-container fill-height fluid>
                         <v-layout fill-height>
                             <v-flex xs12 align-end flexbox>
-                                <v-img class="avator" height="64" width="64" src="https://pan.zealsay.com/d01373f082025aaf26d82daff3edab64024f1aef.jpg"></v-img>
+                                <v-img class="avator" height="64" width="64"
+                                       src="https://pan.zealsay.com/d01373f082025aaf26d82daff3edab64024f1aef.jpg"></v-img>
                             </v-flex>
                         </v-layout>
                     </v-container>
                 </v-img>
                 <v-card-title>
-                    <div>
-                        <span class="grey--text">21 岁</span>
+                    <div class="row center">
+                        <span class="teal--text xs4">{{ row.age }} 岁</span>
                         <v-divider
                                 class="mx-3"
+                                dark
                                 inset
                                 vertical
                         ></v-divider>
-                        <span>男</span>
+                        <span class="xs4">男</span>
                         <v-divider
                                 class="mx-3"
+                                dark
                                 inset
                                 vertical
                         ></v-divider>
-                        <span>zeal</span>
+                        <span class="xs4">zeal</span>
                     </div>
                 </v-card-title>
                 <v-card-actions>
@@ -44,9 +47,21 @@
 <script>
     export default {
         name: "info",
-        data () {
+        props: {
+            row:{
+                type: Object,
+                default: function () {
+                    return {
+                        age:'',
+                        sex:''
+                    }
+                }
+            }
+        },
+        data() {
             return {
-                card_text: 'Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat.'
+                age: 12,
+                sex: '男'
             }
         }
     }
@@ -56,5 +71,9 @@
     .avator {
         margin: 60px auto auto;
         border-radius: 50%;
+    }
+
+    .center {
+        margin: 0 auto;
     }
 </style>
--- src/views/userList/index.vue
+++ src/views/userList/index.vue
@@ -1,186 +1,193 @@
 <template>
-  <div class="con">
-    <v-layout
-      row
-      wrap
-    >
-      <v-flex
-        xs12
-        sm6
-        md3
-      >
-        <v-text-field
-          label="Solo"
-          v-model="phone"
-          placeholder="手机号码"
-          solo
-        ></v-text-field>
-      </v-flex>
-      <v-flex
-        xs12
-        sm6
-        md3
-      >
-        <v-text-field
-          label="Solo"
-          v-model="email"
-          placeholder="邮箱"
-          solo
-        ></v-text-field>
-      </v-flex>
-      <v-flex
-        xs6
-        sm2
-        md2
-      >
-        <v-btn color="info">搜索</v-btn>
-      </v-flex>
-      <v-flex
-        xs12
-        sm6
-        md3
-      >
-        <v-btn
-          color="info"
-        >添加</v-btn>
-        <v-btn color="info">删除</v-btn>
-      </v-flex>
-    </v-layout>
-    <v-data-table
-      :headers="headers"
-      :items="desserts"
-      class="elevation-1"
-    >
-      <template
-        slot="items"
-        slot-scope="props"
-      >
-        <td>
-          <v-avatar
-                  size="32px"
-                  color="grey lighten-4"
-          >
-            <v-img :lazy-src="props.item.avatar" :src="props.item.avatar" alt="avatar"></v-img>
-          </v-avatar>
-        </td>
-        <td>{{ props.item.username }}</td>
-        <td>{{ props.item.name }}</td>
-        <td v-if="props.item.sex==0"> <img width="24px" src="../../assets/sex/boy.png"/></td>
-        <td v-if="props.item.sex==1"> <img width="24px" src="../../assets/sex/girl.png"/></td>
-        <td>{{ props.item.age }}</td>
-        <td>{{ props.item.email }}</td>
-        <td v-if="props.item.status == 'NORMAL'" class="text-info">正常</td>
-        <td v-if="props.item.status == 'DISABLED'" class="text-error">禁用</td>
-        <td v-if="props.item.status == 'LOCK'" class="text-warning">锁定</td>
-        <td>
-          <v-layout
-            justify-space-around
-            class="mb-2"
-          >
-            <v-icon @click="handleInfo(props.item)">fa-vcard</v-icon>
-            <v-icon @click="handleEdit('编辑',props.item)">fa-edit</v-icon>
-          </v-layout>
-        </td>
-      </template>
-    </v-data-table>
+    <div class="con">
+        <v-layout
+                row
+                wrap
+        >
+            <v-flex
+                    xs12
+                    sm6
+                    md3
+            >
+                <v-text-field
+                        label="Solo"
+                        v-model="phone"
+                        placeholder="手机号码"
+                        solo
+                ></v-text-field>
+            </v-flex>
+            <v-flex
+                    xs12
+                    sm6
+                    md3
+            >
+                <v-text-field
+                        label="Solo"
+                        v-model="email"
+                        placeholder="邮箱"
+                        solo
+                ></v-text-field>
+            </v-flex>
+            <v-flex
+                    xs6
+                    sm2
+                    md2
+            >
+                <v-btn color="info">搜索</v-btn>
+            </v-flex>
+            <v-flex
+                    xs12
+                    sm6
+                    md3
+            >
+                <v-btn
+                        color="info"
+                >添加
+                </v-btn>
+                <v-btn color="info">删除</v-btn>
+            </v-flex>
+        </v-layout>
+        <v-data-table
+                :headers="headers"
+                :items="desserts"
+                class="elevation-1"
+        >
+            <template
+                    slot="items"
+                    slot-scope="props"
+            >
+                <td>
+                    <v-avatar
+                            size="32px"
+                            color="grey lighten-4"
+                    >
+                        <v-img :lazy-src="props.item.avatar" :src="props.item.avatar" alt="avatar"></v-img>
+                    </v-avatar>
+                </td>
+                <td>{{ props.item.username }}</td>
+                <td>{{ props.item.name }}</td>
+                <td v-if="props.item.sex==0"><img width="24px" src="../../assets/sex/boy.png"/></td>
+                <td v-if="props.item.sex==1"><img width="24px" src="../../assets/sex/girl.png"/></td>
+                <td>{{ props.item.age }}</td>
+                <td>{{ props.item.email }}</td>
+                <td v-if="props.item.status == 'NORMAL'" class="text-info">正常</td>
+                <td v-if="props.item.status == 'DISABLED'" class="text-error">禁用</td>
+                <td v-if="props.item.status == 'LOCK'" class="text-warning">锁定</td>
+                <td>
+                    <v-layout
+                            justify-space-around
+                            class="mb-2"
+                    >
+                        <v-icon @click="handleInfo(props.item)">fa-vcard</v-icon>
+                        <v-icon @click="handleEdit('编辑',props.item)">fa-edit</v-icon>
+                    </v-layout>
+                </td>
+            </template>
+        </v-data-table>
 
-
-  </div>
+    </div>
 </template>
 <script>
-import { getUserList } from "@/api/user";
-import forms from './components/form'
-import info from './components/info'
+    import {getUserList} from "@/api/user";
+    import forms from './components/form'
+    import info from './components/info'
 
-export default {
-    name: "User",
-  components: { forms,info },
-  data() {
-    return {
-      phone: "",
-      email: "",
-      headers: [
-        {
-          text: "头像",
-          value: "avatar"
+    export default {
+        name: "User",
+        components: {forms, info},
+        data() {
+            return {
+                phone: "",
+                email: "",
+                headers: [
+                    {
+                        text: "头像",
+                        value: "avatar"
+                    },
+                    {
+                        text: "用户名",
+                        value: "username"
+                    },
+                    {text: "姓名", value: "name"},
+                    {text: "性别", value: "sex"},
+                    {text: "年龄", value: "age"},
+                    {text: "邮箱", value: "email"},
+                    {text: "状态", value: "status"},
+                    {text: "操作", value: ""}
+                ],
+                desserts: [],
+                row: {},
+                dialogVisible: false,
+                formVisible: false,
+                title: ""
+            };
         },
-        {
-          text: "用户名",
-          value: "username"
+        created() {
+            getUserList().then(res => {
+                this.desserts = res.data.records;
+            });
         },
-        { text: "姓名", value: "name" },
-        { text: "性别", value: "sex" },
-        { text: "年龄", value: "age" },
-        { text: "邮箱", value: "email" },
-        { text: "状态", value: "status" },
-        { text: "操作", value: "" }
-      ],
-      desserts: [],
-      row: {},
-      dialogVisible: false,
-      formVisible: false,
-      title: ""
+        methods: {
+            succModel() {
+                console.log(111)
+                this.formVisible = false;
+            },
+            sumForm() {
+                this.$refs.addForm.submitForm('ruleForm')
+            },
+            handleEdit(title, row) {
+                this.title = title;
+                if (row) {
+                    this.row = row;
+                }
+                this.formVisible = true;
+                console.log(this.row);
+            },
+            handleInfo(row) {
+                this.row = row;
+                console.log(this.row);
+                const res = this.$dialog.show(info)
+            },
+        }
     };
-  },
-  created() {
-    getUserList().then(res => {
-      this.desserts = res.data.records;
-    });
-  },
-  methods: {
-    succModel(){
-        console.log(111)
-      this.formVisible=false;
-    },
-    sumForm(){
-      this.$refs.addForm.submitForm('ruleForm')
-    },
-    handleEdit(title, row) {
-      this.title = title;
-      if (row) {
-        this.row = row;
-      }
-      this.formVisible = true;
-      console.log(this.row);
-    },
-    handleInfo(row) {
-      this.row = row;
-      console.log(this.row);
-      const res =  this.$dialog.show(info)
-    },
-  }
-};
 </script>
 <style lang="less" scoped>
-.con {
-  padding: 0 20px;
-}
-.layout > .flex {
-  padding: 8px;
-}
-.v-icon{
-    color: #2196f3;
-    cursor: pointer;
-}
-.img {
-  width: 30px;
-  height: 30px;
-}
-.v-image__image {
-  width: 30px;
-  height: 30px;
-}
-.text-info {
-  color: forestgreen;
-  font-weight: bold;
-}
-.text-error {
-  color: #dc6752;
-  font-weight: bold;
-}
-.text-warning {
-  color: #dca173;
-  font-weight: bold;
-}
+    .con {
+        padding: 0 20px;
+    }
+
+    .layout > .flex {
+        padding: 8px;
+    }
+
+    .v-icon {
+        color: #2196f3;
+        cursor: pointer;
+    }
+
+    .img {
+        width: 30px;
+        height: 30px;
+    }
+
+    .v-image__image {
+        width: 30px;
+        height: 30px;
+    }
+
+    .text-info {
+        color: forestgreen;
+        font-weight: bold;
+    }
+
+    .text-error {
+        color: #dc6752;
+        font-weight: bold;
+    }
+
+    .text-warning {
+        color: #dca173;
+        font-weight: bold;
+    }
 </style>
 
