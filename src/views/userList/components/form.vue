de12adf5d296ea7bba97f39e530ea045d1a8285d
zeal <zhanglei@zealsay.com>
2019-03-14 12:08:46 +0800

    [feat] 调整edit页面的样式

--- src/views/userList/components/form.vue
+++ src/views/userList/components/form.vue
@@ -1,63 +1,55 @@
 <template>
-    <v-layout>
-    <v-form
-            ref="form"
-            v-model="valid"
-            lazy-validation
-    >
-        <v-text-field
-                v-model="name"
-                :counter="10"
-                :rules="nameRules"
-                label="Name"
-                required
-        ></v-text-field>
-
-        <v-text-field
-                v-model="email"
-                :rules="emailRules"
-                label="E-mail"
-                required
-        ></v-text-field>
-
-        <v-select
-                v-model="select"
-                :items="items"
-                :rules="[v => !!v || 'Item is required']"
-                label="Item"
-                required
-        ></v-select>
-
-        <v-checkbox
-                v-model="checkbox"
-                :rules="[v => !!v || 'You must agree to continue!']"
-                label="Do you agree?"
-                required
-        ></v-checkbox>
-
-        <v-btn
-                :disabled="!valid"
-                color="success"
-                @click="validate"
-        >
-            Validate
-        </v-btn>
-
-        <v-btn
-                color="error"
-                @click="reset"
-        >
-            Reset Form
-        </v-btn>
-
-        <v-btn
-                color="warning"
-                @click="resetValidation"
-        >
-            Reset Validation
-        </v-btn>
-    </v-form>
-    </v-layout>
+    <v-card>
+        <v-card-title>
+            <span class="headline">User Profile</span>
+        </v-card-title>
+        <v-card-text>
+            <v-container grid-list-md>
+                <v-layout wrap>
+                    <v-flex xs12 sm6 md4>
+                        <v-text-field label="Legal first name*" required></v-text-field>
+                    </v-flex>
+                    <v-flex xs12 sm6 md4>
+                        <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
+                    </v-flex>
+                    <v-flex xs12 sm6 md4>
+                        <v-text-field
+                                label="Legal last name*"
+                                hint="example of persistent helper text"
+                                persistent-hint
+                                required
+                        ></v-text-field>
+                    </v-flex>
+                    <v-flex xs12>
+                        <v-text-field label="Email*" required></v-text-field>
+                    </v-flex>
+                    <v-flex xs12>
+                        <v-text-field label="Password*" type="password" required></v-text-field>
+                    </v-flex>
+                    <v-flex xs12 sm6>
+                        <v-select
+                                :items="['0-17', '18-29', '30-54', '54+']"
+                                label="Age*"
+                                required
+                        ></v-select>
+                    </v-flex>
+                    <v-flex xs12 sm6>
+                        <v-autocomplete
+                                :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                                label="Interests"
+                                multiple
+                        ></v-autocomplete>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+            <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" flat @click="dialog = false">Close</v-btn>
+            <v-btn color="blue darken-1" flat @click="dialog = false">Save</v-btn>
+        </v-card-actions>
+    </v-card>
 </template>
 
 <script>
