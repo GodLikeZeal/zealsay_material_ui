9c734664fd0a7201ef425597e8604199f4e65d98
zeal <zhanglei@zealsay.com>
2019-03-20 22:52:06 +0800

    [feat] 调整左侧菜单栏

--- src/components/core/Drawer.vue
+++ src/components/core/Drawer.vue
@@ -42,21 +42,76 @@
             color="purple"
           />
         </v-list-tile>
-        <v-list-tile
-          v-for="(link, i) in links"
-          :key="i"
-          :to="link.to"
-          :active-class="color"
-          avatar
-          class="v-list-item"
-        >
-          <v-list-tile-action>
-            <v-icon>{{ link.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title
-            v-text="link.text"
-          />
-        </v-list-tile>
+        <!--<v-list-tile-->
+          <!--v-for="(link, i) in links"-->
+          <!--:key="i"-->
+          <!--:to="link.name"-->
+          <!--:active-class="color"-->
+          <!--avatar-->
+          <!--class="v-list-item"-->
+        <!--&gt;-->
+          <!--<v-list-tile-action>-->
+            <!--<v-icon>{{ link.icon }}</v-icon>-->
+          <!--</v-list-tile-action>-->
+          <!--<v-list-tile-title-->
+            <!--v-text="link.text"-->
+          <!--/>-->
+        <!--</v-list-tile>-->
+        <vue-perfect-scrollbar class="drawer-menu--scroll" :settings="scrollSettings">
+          <v-list dense expand>
+            <template v-for="(item, i) in menus">
+              <!--group with subitems-->
+              <v-list-group v-if="item.items" :key="item.name" :group="item.group" :prepend-icon="item.icon" no-action="no-action">
+                <v-list-tile slot="activator" ripple="ripple">
+                  <v-list-tile-content>
+                    <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                  </v-list-tile-content>
+                </v-list-tile>
+                <template v-for="(subItem, i) in item.items">
+                  <!--sub group-->
+                  <v-list-group v-if="subItem.items" :key="subItem.name" :group="subItem.group" sub-group="sub-group">
+                    <v-list-tile slot="activator" ripple="ripple">
+                      <v-list-tile-content>
+                        <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
+                      </v-list-tile-content>
+                    </v-list-tile>
+                    <v-list-tile v-for="(grand, i) in subItem.children" :key="i" :to="genChildTarget(item, grand)" :href="grand.href" ripple="ripple">
+                      <v-list-tile-content>
+                        <v-list-tile-title>{{ grand.title }}</v-list-tile-title>
+                      </v-list-tile-content>
+                    </v-list-tile>
+                  </v-list-group>
+                  <!--child item-->
+                  <v-list-tile v-else :key="i" :to="genChildTarget(item, subItem)" :href="subItem.href" :disabled="subItem.disabled" :target="subItem.target" ripple="ripple">
+                    <v-list-tile-content>
+                      <v-list-tile-title><span>{{ subItem.title }}</span></v-list-tile-title>
+                    </v-list-tile-content>
+                    <!-- <v-circle class="white--text pa-0 circle-pill" v-if="subItem.badge" color="red" disabled="disabled">{{ subItem.badge }}</v-circle> -->
+                    <v-list-tile-action v-if="subItem.action">
+                      <v-icon :class="[subItem.actionClass || 'success--text']">{{ subItem.action }}</v-icon>
+                    </v-list-tile-action>
+                  </v-list-tile>
+                </template>
+              </v-list-group>
+              <v-subheader v-else-if="item.header" :key="i">{{ item.header }}</v-subheader>
+              <v-divider v-else-if="item.divider" :key="i"></v-divider>
+              <!--top-level link-->
+              <v-list-tile v-else :to="!item.href ? { name: item.name } : null" :href="item.href" ripple="ripple" :disabled="item.disabled" :target="item.target" rel="noopener" :key="item.name">
+                <v-list-tile-action v-if="item.icon">
+                  <v-icon>{{ item.icon }}</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                  <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                </v-list-tile-content>
+                <!-- <v-circle class="white--text pa-0 chip--x-small" v-if="item.badge" :color="item.color || 'primary'" disabled="disabled">{{ item.badge }}</v-circle> -->
+                <v-list-tile-action v-if="item.subAction">
+                  <v-icon class="success--text">{{ item.subAction }}</v-icon>
+                </v-list-tile-action>
+                <!-- <v-circle class="caption blue lighten-2 white--text mx-0" v-else-if="item.chip" label="label" small="small">{{ item.chip }}</v-circle> -->
+              </v-list-tile>
+            </template>
+          </v-list>
+        </vue-perfect-scrollbar>
 
       </v-layout>
     </v-img>
@@ -69,48 +124,20 @@ import {
   mapMutations,
   mapState
 } from 'vuex'
+import menu from '@/api/menu';
+import VuePerfectScrollbar from 'vue-perfect-scrollbar';
 
 export default {
+  components: {
+    VuePerfectScrollbar,
+  },
   data: () => ({
     logo: '/cat.png',
-    links: [
-      {
-        to: '/dashboard',
-        icon: 'mdi-view-dashboard',
-        text: 'Dashboard'
-      },
-      {
-        to: '/user-profile',
-        icon: 'mdi-account',
-        text: 'User Profile'
-      },
-      {
-        to: '/table-list',
-        icon: 'mdi-clipboard-outline',
-        text: 'Table List'
-      },
-      {
-        to: '/typography',
-        icon: 'mdi-format-font',
-        text: 'Typography'
-      },
-      {
-        to: '/icons',
-        icon: 'mdi-chart-bubble',
-        text: 'Icons'
-      },
-      {
-        to: '/maps',
-        icon: 'mdi-map-marker',
-        text: 'Maps'
-      },
-      {
-        to: '/notifications',
-        icon: 'mdi-bell',
-        text: 'Notifications'
-      }
-    ],
-    responsive: false
+    menus: menu,
+    responsive: false,
+    scrollSettings: {
+      maxScrollbarLength: 160
+    }
   }),
   computed: {
     ...mapState('app', ['image', 'color']),
@@ -124,6 +151,16 @@ export default {
     },
     items () {
       return this.$t('Layout.View.items')
+    },
+    computeGroupActive () {
+      return true;
+    },
+    computeLogo () {
+      return '/static/m.png';
+    },
+
+    sideToolbarColor () {
+      return this.$vuetify.options.extra.sideNav;
     }
   },
   mounted () {
@@ -141,6 +178,15 @@ export default {
       } else {
         this.responsive = false
       }
+    },
+    genChildTarget (item, subItem) {
+      if (subItem.href) return;
+      if (subItem.component) {
+        return {
+          name: subItem.component,
+        };
+      }
+      return { name: `${item.group}/${(subItem.name)}` };
     }
   }
 }
