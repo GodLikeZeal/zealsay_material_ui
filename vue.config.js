2fea491b81887381d5c8e6acdd13e89657ce60ea
zhanglei <zhanglei@i-counting.cn>
2019-03-22 10:59:34 +0800

    [feat] 解决线上部署样式和本地不一致的问题

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  runtimeCompiler: true,
+
+  css: {
+    extract: false
+  },
+
+  publicPath: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined
+};
